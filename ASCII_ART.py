d3cac45e22a9d84f4a3dc612179c0f61387e8813
Dhanraj Kumar <dhanrajk046@gmail.com>
2026-01-19 18:35:29 +0530

    Update ASCII_ART.py with a poem
    Replaced ASCII art with the poem 'Twinkle, Twinkle, Little Star'.

--- ASCII_ART.py
+++ ASCII_ART.py
@@ -1,32 +1,26 @@
-print('''                                                       _..  
-                                                   .qd$$$$bp.
-                                                 .q$$$$$$$$$$m.
-                                                .$$$$$$$$$$$$$$
-                                              .q$$$$$$$$$$$$$$$$
-                                             .$$$$$$$$$$$$P\$$$$;
-                                           .q$$$$$$$$$P^"_.`;$$$$
-                                          q$$$$$$$P;\   ,  /$$$$P
-                                        .$$$P^::Y$/`  _  .:.$$$/
-                                       .P.:..    \ `._.-:.. \$P
-                                       $':.  __.. :   :..    :'
-                                      /:_..::.   `. .:.    .'|
-                                    _::..          T:..   /  :
-                                 .::..             J:..  :  :
-                              .::..          7:..   F:.. :  ;
-                          _.::..             |:..   J:.. `./
-                     _..:::..               /J:..    F:.  : 
-                   .::::..                .T  \:..   J:.  /
-                  /:::...               .' `.  \:..   F_o'
-                 .:::...              .'     \  \:..  J ;
-                 ::::...           .-'`.    _.`._\:..  \'
-                 ':::...         .'  `._7.-'_.-  `\:.   \
-                  \:::...   _..-'__.._/_.--' ,:.   b:.   \._ 
-                   `::::..-"_.'-"_..--"      :..   /):.   `.\   
-                     `-:/"-7.--""            _::.-'P::..    \} 
-          _....------""""""            _..--".-'   \::..     `. 
-         (::..              _...----"""  _.-'       `---:..    `-.
-          \::..      _.-""""   `""""---""                `::...___)
-           `\:._.-"""                             fsc
+print('''                            
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
 
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
 
+Then the traveler in the dark
+Thanks you for your tiny spark,
+How could he see where to go,
+If you did not twinkle so?
+
+In the dark blue sky you keep,
+Often through my curtains peep
+For you never shut your eye,
+Till the sun is in the sky.
+
+As your bright and tiny spark
+Lights the traveler in the dark,
+Though I know not what you are,
+Twinkle, twinkle, little star.
       ''')
